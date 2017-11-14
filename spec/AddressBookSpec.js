07e4fc0285eb2d3d66fff54740bd8f53a4abb11f
Evgenii Fedoseev <abap@sapville.com>
2017-11-14 16:40:07 +0300

    test: add AddressBook async test suite

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -4,11 +4,44 @@ Contact
  */
 
 describe('AddressBook', function () {
-  it('should be able to add a contact', function () {
-    const addressBook = new AddressBook();
-    const thisContact = new Contact();
 
+  let addressBook;
+  let thisContact;
+
+  beforeEach(function () {
+    addressBook = new AddressBook();
+    thisContact = new Contact();
+  });
+
+  it('should be able to add a contact', function () {
     addressBook.addContact(thisContact);
     expect(addressBook.getContact(0)).toBe(thisContact);
+  });
+
+  it('should be able to delete a contact', function () {
+    addressBook.addContact(thisContact);
+    addressBook.deleteContact(0);
+
+    expect(addressBook.getContact(0)).not.toBeDefined();
+  });
+
+});
+
+describe('Address Book Async', function () {
+  let addressBook;
+
+  beforeEach(function (done) {
+    addressBook = new AddressBook();
+    addressBook.getInitialContacts(done);
+    /*
+        addressBook.getInitialContacts(function () {
+          done();
+        });
+    */
+  });
+
+  it('should download initial content of Address Book', function (done) {
+    expect(addressBook.initialComplete).toBe(true);
+    done();
   });
 });
--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,15 +1,25 @@
-/*global
-Contact
- */
-
-class AddressBook {
+class AddressBook { //eslint-disable-line no-unused-vars
   constructor () {
+    this.initialComplete = false;
     this.contacts = [];
   }
+
+  getInitialContacts (cb) {
+    setTimeout(() => {
+      this.initialComplete = true;
+      if (cb) {return cb();}
+    }, 10);
+  }
+
   addContact (contact) {
     this.contacts.push(contact);
   }
-  getContact(number) {
+
+  getContact (number) {
     return this.contacts[number];
+  }
+
+  deleteContact (number) {
+    this.contacts.splice(number, 1);
   }
 }
